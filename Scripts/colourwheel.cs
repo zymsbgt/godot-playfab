99343da17448125df18dd7759068da78cefa82b8
zymsbgt <91821082+zymsbgt@users.noreply.github.com>
2023-01-01 22:39:23 +0800

    ColourWheel migration successful!

--- Scripts/colourwheel.cs
+++ Scripts/colourwheel.cs
@@ -1,17 +1,17 @@
 using Godot;
 using System;
 
-public class colourwheel : Area2D
+public class ColourWheel : ControllerWheel
 {
     private AnimatedSprite _animatedSprite;
-    private bool queuePlay = false, mouseDown = false, joystickMoved = false;
+    private bool queuePlay = false;
     [Signal] delegate void disable_player_movement(bool state);
 
     public override void _Ready()
     {
         _animatedSprite = GetNode<AnimatedSprite>("AnimatedSprite");
         _animatedSprite.Play("passive");
-        this.Visible = false;
+        SetVisibility(false);
     }
 
     public void _on_area_entered(Area2D area)
@@ -24,34 +24,17 @@ public class colourwheel : Area2D
         _animatedSprite.Play("passive");
     }
 
-    public override void _Input(InputEvent @event)
-    {
-        if (@event is InputEventMouseButton eventMouseButton)
-        {
-            if (eventMouseButton.IsPressed())
-            {
-                mouseDown = true;
-                SetVisibility(true);
-            }
-            else
-            {
-                mouseDown = false;
-                if (!joystickMoved)
-                    SetVisibility(false);
-            }    
-        }
-    }
-
-    public void SetVisibility(bool visibility)
+    public override void SetVisibility(bool visibility)
     {
         if (visibility == false)
             _animatedSprite.Play("passive");
-        this.Visible = visibility;
+        base.SetVisibility(visibility);
     }
 
     public override void _Process(float delta)
     {
-        // queueplay
+        base._Process(delta);
+
         if (queuePlay)
         {
             GetNode<AudioStreamPlayer2D>("AudioStreamPlayer2D").Play();
@@ -61,21 +44,5 @@ public class colourwheel : Area2D
                 EmitSignal("disable_player_movement", false);
         }
 	    queuePlay = false;
-
-        // controller
-        float doNotTriggerBelow = 0.01f;
-        float x = Input.GetActionStrength("sing_right_controller") - Input.GetActionStrength("sing_left_controller");
-        float y = Input.GetActionStrength("sing_down_controller") - Input.GetActionStrength("sing_up_controller");
-        if (x > doNotTriggerBelow || y > doNotTriggerBelow || x < -doNotTriggerBelow || y < -doNotTriggerBelow)
-        {
-            joystickMoved = true;
-            SetVisibility(true);
-        }
-        else
-        {
-            joystickMoved = false;
-            if (mouseDown == false)
-                SetVisibility(false);
-        }
     }
 }
