fe71ecd43ff0e9bbd19e4192662d974a7695cd63
zymsbgt <91821082+zymsbgt@users.noreply.github.com>
2023-01-28 22:36:57 +0800

    Edited warning for HTML5

--- Scenes/MOTD.gd
+++ Scenes/MOTD.gd
@@ -4,7 +4,6 @@ func _ready() -> void:
 	visible = false
 	if OS.get_name() == "HTML5":
 		visible = true
-		text = "Heads up! This web version of the game is provided for convenience and may contain bugs.\nIf you've encountered a bug, please check if it's present on the Windows/Linux versions before reporting!"
 	if OS.get_model_name() != "GenericDevice":
 		visible = true
 		text = "Warning: " + OS.get_model_name() + " device detected.\nThis game is designed for desktops and laptops only."
